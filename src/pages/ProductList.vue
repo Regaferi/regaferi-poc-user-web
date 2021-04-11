7f920903aa3741ab3dfb3d79cee851beb94dfadf
RainyBlossom <bo.wu@baozuncom>
2021-04-11 14:41:56 +0800

    update for plp

--- src/pages/ProductList.vue
+++ src/pages/ProductList.vue
@@ -27,7 +27,7 @@
       <v-spacer></v-spacer>
     </v-row>
     <!--  List Detail  -->
-    <v-row class="pt-3">
+    <v-row>
       <v-col cols="6">
           <v-card class="pl-2">
             <v-img
@@ -55,7 +55,7 @@
           </v-card>
       </v-col>
       <v-col cols="6">
-          <v-card class="pl-2">
+          <v-card class="pr-2">
             <v-img
                 height="180"
                 src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
@@ -80,161 +80,167 @@
             </v-card-text>
           </v-card>
       </v-col>
+    </v-row>
+    <v-row>
       <v-col cols="6">
-          <v-card class="pl-2">
-            <v-img
-                height="180"
-                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-            ></v-img>
-            <v-card-title>Cafe Badilico</v-card-title>
-            <v-card-text>
-              <v-row
-                  align="center"
-                  class="mx-0 pb-3"
-              >
-                <v-rating
-                    :value="4.5"
-                    color="amber"
-                    dense
-                    half-increments
-                    readonly
-                    size="14"
-                ></v-rating>
-              </v-row>
-              <h6> $ • Italian, Cafe</h6>
-              <h6>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</h6>
-            </v-card-text>
-          </v-card>
+        <v-card class="pl-2">
+          <v-img
+              height="180"
+              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+          ></v-img>
+          <v-card-title>Cafe Badilico</v-card-title>
+          <v-card-text>
+            <v-row
+                align="center"
+                class="mx-0 pb-3"
+            >
+              <v-rating
+                  :value="4.5"
+                  color="amber"
+                  dense
+                  half-increments
+                  readonly
+                  size="14"
+              ></v-rating>
+            </v-row>
+            <h6> $ • Italian, Cafe</h6>
+            <h6>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</h6>
+          </v-card-text>
+        </v-card>
       </v-col>
       <v-col cols="6">
-          <v-card class="pl-2">
-            <v-img
-                height="180"
-                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-            ></v-img>
-            <v-card-title>Cafe Badilico</v-card-title>
-            <v-card-text>
-              <v-row
-                  align="center"
-                  class="mx-0 pb-3"
-              >
-                <v-rating
-                    :value="4.5"
-                    color="amber"
-                    dense
-                    half-increments
-                    readonly
-                    size="14"
-                ></v-rating>
-              </v-row>
-              <h6> $ • Italian, Cafe</h6>
-              <h6>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</h6>
-            </v-card-text>
-          </v-card>
+        <v-card class="pr-2">
+          <v-img
+              height="180"
+              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+          ></v-img>
+          <v-card-title>Cafe Badilico</v-card-title>
+          <v-card-text>
+            <v-row
+                align="center"
+                class="mx-0 pb-3"
+            >
+              <v-rating
+                  :value="4.5"
+                  color="amber"
+                  dense
+                  half-increments
+                  readonly
+                  size="14"
+              ></v-rating>
+            </v-row>
+            <h6> $ • Italian, Cafe</h6>
+            <h6>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</h6>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="6">
+        <v-card class="pl-2">
+          <v-img
+              height="180"
+              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+          ></v-img>
+          <v-card-title>Cafe Badilico</v-card-title>
+          <v-card-text>
+            <v-row
+                align="center"
+                class="mx-0 pb-3"
+            >
+              <v-rating
+                  :value="4.5"
+                  color="amber"
+                  dense
+                  half-increments
+                  readonly
+                  size="14"
+              ></v-rating>
+            </v-row>
+            <h6> $ • Italian, Cafe</h6>
+            <h6>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</h6>
+          </v-card-text>
+        </v-card>
       </v-col>
       <v-col cols="6">
-          <v-card class="pl-2">
-            <v-img
-                height="180"
-                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-            ></v-img>
-            <v-card-title>Cafe Badilico</v-card-title>
-            <v-card-text>
-              <v-row
-                  align="center"
-                  class="mx-0 pb-3"
-              >
-                <v-rating
-                    :value="4.5"
-                    color="amber"
-                    dense
-                    half-increments
-                    readonly
-                    size="14"
-                ></v-rating>
-              </v-row>
-              <h6> $ • Italian, Cafe</h6>
-              <h6>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</h6>
-            </v-card-text>
-          </v-card>
+        <v-card class="pr-2">
+          <v-img
+              height="180"
+              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+          ></v-img>
+          <v-card-title>Cafe Badilico</v-card-title>
+          <v-card-text>
+            <v-row
+                align="center"
+                class="mx-0 pb-3"
+            >
+              <v-rating
+                  :value="4.5"
+                  color="amber"
+                  dense
+                  half-increments
+                  readonly
+                  size="14"
+              ></v-rating>
+            </v-row>
+            <h6> $ • Italian, Cafe</h6>
+            <h6>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</h6>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="6">
+        <v-card class="pl-2">
+          <v-img
+              height="180"
+              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+          ></v-img>
+          <v-card-title>Cafe Badilico</v-card-title>
+          <v-card-text>
+            <v-row
+                align="center"
+                class="mx-0 pb-3"
+            >
+              <v-rating
+                  :value="4.5"
+                  color="amber"
+                  dense
+                  half-increments
+                  readonly
+                  size="14"
+              ></v-rating>
+            </v-row>
+            <h6> $ • Italian, Cafe</h6>
+            <h6>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</h6>
+          </v-card-text>
+        </v-card>
       </v-col>
       <v-col cols="6">
-          <v-card class="pl-2">
-            <v-img
-                height="180"
-                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-            ></v-img>
-            <v-card-title>Cafe Badilico</v-card-title>
-            <v-card-text>
-              <v-row
-                  align="center"
-                  class="mx-0 pb-3"
-              >
-                <v-rating
-                    :value="4.5"
-                    color="amber"
-                    dense
-                    half-increments
-                    readonly
-                    size="14"
-                ></v-rating>
-              </v-row>
-              <h6> $ • Italian, Cafe</h6>
-              <h6>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</h6>
-            </v-card-text>
-          </v-card>
-      </v-col>
-      <v-col cols="6">
-          <v-card class="pl-2">
-            <v-img
-                height="180"
-                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-            ></v-img>
-            <v-card-title>Cafe Badilico</v-card-title>
-            <v-card-text>
-              <v-row
-                  align="center"
-                  class="mx-0 pb-3"
-              >
-                <v-rating
-                    :value="4.5"
-                    color="amber"
-                    dense
-                    half-increments
-                    readonly
-                    size="14"
-                ></v-rating>
-              </v-row>
-              <h6> $ • Italian, Cafe</h6>
-              <h6>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</h6>
-            </v-card-text>
-          </v-card>
-      </v-col>
-      <v-col cols="6">
-          <v-card class="pl-2">
-            <v-img
-                height="180"
-                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-            ></v-img>
-            <v-card-title>Cafe Badilico</v-card-title>
-            <v-card-text>
-              <v-row
-                  align="center"
-                  class="mx-0 pb-3"
-              >
-                <v-rating
-                    :value="4.5"
-                    color="amber"
-                    dense
-                    half-increments
-                    readonly
-                    size="14"
-                ></v-rating>
-              </v-row>
-              <h6> $ • Italian, Cafe</h6>
-              <h6>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</h6>
-            </v-card-text>
-          </v-card>
+        <v-card class="pr-2">
+          <v-img
+              height="180"
+              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+          ></v-img>
+          <v-card-title>Cafe Badilico</v-card-title>
+          <v-card-text>
+            <v-row
+                align="center"
+                class="mx-0 pb-3"
+            >
+              <v-rating
+                  :value="4.5"
+                  color="amber"
+                  dense
+                  half-increments
+                  readonly
+                  size="14"
+              ></v-rating>
+            </v-row>
+            <h6> $ • Italian, Cafe</h6>
+            <h6>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</h6>
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </v-main>
