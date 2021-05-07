124c119df4681f98a3e8326347457f3be6823f59
U672451 <U672451@NTTD-10VD41159.bxo.local>
2021-05-07 18:29:36 +0900

    主页样式修改

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <v-main>
+	<v-banner elevation="3" style="margin:7px;padding: 0; background: #f3f0e9;">
     <!-- CMS 轮播开始 -->
     <v-carousel
         cycle
@@ -16,7 +17,8 @@
       >
         <v-sheet
             :color="colors[i]"
-            height="100%"
+            height="100%"         
+             class="mx-auto transition-swing secondary"
         >
           <v-row
               class="fill-height"
@@ -30,36 +32,47 @@
         </v-sheet>
       </v-carousel-item>
     </v-carousel>
+        </v-banner>
     <!-- CMS 轮播结束 -->
+
     <!-- 引导菜单开始 -->
-    <v-row style="width: 60%; margin: auto; left: 0; right: 0;">
-      <v-col cols="4"
+    <v-row style="width: 100%; margin: auto; left: 0; right: 0;">
+      <v-col :cols="isMobile?'6':'2'" 
              v-for="(navigator, i) in navigators"
              :key="i"
              @click="navigateTo(navigator.navigator.name, navigator.navigator.category, navigator.navigator.property)"
       >
-        <v-card :width="isMobile?'50px':'220px'" :height="isMobile?'50px':'80px'">
-          <v-row class="navigator-row" v-show="!isMobile">
-            <v-col cols="4" style="margin: auto; top: 15%"><v-icon :size="35">{{navigator.icon}}</v-icon></v-col>
-            <v-col cols="8"><v-card-subtitle>{{navigator.title}}</v-card-subtitle></v-col>
+      <v-banner elevation="3" style="margin:auto;padding: auto; background: #f3f0e9;">
+        <v-card :width="isMobile?'100%':'100%'" :height="isMobile?'100%':'300px'"  >
+         <v-img :lazy-src="navigator.imageurl" max-height=100% max-width=100% :src="navigator.imageurl" ></v-img>
+          <v-row class="navigator-row"  >     
+              <v-card-subtitle  class="font-weight-black" style="margin:auto;padding:auto; ">{{navigator.title}}</v-card-subtitle>
           </v-row>
-          <v-icon class="navigator-row-mb" :size="30" v-show="isMobile">{{navigator.icon}}</v-icon>
-        </v-card>
+        </v-card> 
+        </v-banner>
       </v-col>
     </v-row>
     <!-- 引导菜单结束 -->
-    <!-- Store Locator -->
+         <!-- SHARE -->
+  <v-banner elevation="3" style="margin:7px;padding: 0; background: #f3f0e9;">
+    <v-card class="text-center" style="text-align: center">
+      <Introduction/>
+    </v-card>
+     </v-banner>
+        <!-- Store Locator -->
+  <v-banner elevation="3" style="margin:7px;padding: 3px; background: #f3f0e9;">
     <v-card class="text-center" style="text-align: center">
       <v-card-title>近くのレストランを探す</v-card-title>
       <GoogleMap/>
     </v-card>
-
+    </v-banner> 
   </v-main>
 </template>
 
 <script>
 
 import GoogleMap from "@/components/GoogleMap";
+import Introduction from "@/pages/Introduction";
 export default {
   name: "Home",
   data () {
@@ -71,24 +84,54 @@ export default {
           navigator : {
             name: 'product-list',
             url: '/list',
-            category : 'recommend',
+            category : 'recommend',          
             property : ''
-          }
+          },
+           imageurl : 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=4253792690,4157255255&fm=224&gp=0.jpg',
         },
         {
           title : '美食',
           icon: 'mdi-dialpad',
           navigator : {
             name: 'product-list',
-            url: '/list'
-          }
+            url: '/list',
+          },
+          imageurl : 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1992102675,2090288480&fm=26&gp=0.jpg',
         },
         {
           title : '饮品',
           icon: 'mdi-dialpad',
+          imageurl : 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=4276895145,1303738748&fm=26&gp=0.jpg',
           navigator : {
             name: 'product-list',
-            url: '/list'
+            url: '/list',
+          }
+        },
+        {
+          title : '娱乐',
+          icon: 'mdi-dialpad',
+           imageurl : 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=8018056,954128737&fm=26&gp=0.jpg',
+          navigator : {
+            name: 'product-list',
+            url: '/list',
+          }
+        },
+        {
+          title : '温泉',
+          icon: 'mdi-dialpad',
+           imageurl : 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=4253792690,4157255255&fm=224&gp=0.jpg',
+          navigator : {
+            name: 'product-list',
+            url: '/list',
+          }
+        },
+        {
+          title : '旅馆',
+          icon: 'mdi-dialpad',
+          imageurl : 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=220512769,876056645&fm=224&gp=0.jpg',
+          navigator : {
+            name: 'product-list',
+            url: '/list',
           }
         },
       ],
@@ -110,7 +153,8 @@ export default {
     }
   },
   components : {
-    GoogleMap
+    GoogleMap,
+    Introduction
   },
   created() {
 
@@ -129,14 +173,21 @@ export default {
 
 <style scoped>
 .navigator-row {
-  width:180px;
+  width:100%;
   margin: auto;
   left: 0;
   right: 0;
 }
 .navigator-row-mb {
   margin: auto;
-  top: 20%;
+  top: 40%;
   left: 20%
+}
+.navigator-subtitle{
+    margin: auto;
+	position:absolute; 
+	z-index:2; 
+	left:33%;
+	top:40%;
 }
 </style>
---
+++ src/pages/Introduction.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-card
+    max-width="100%"
+
+  >
+    <v-toolbar color="#bf8860" dark>
+      <v-toolbar-title>お知らせ</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+
+    <v-list three-line>
+      <template v-for="(item, index) in items">
+        <v-subheader
+          v-if="item.header"
+          :key="item.header"
+          v-text="item.header"
+        ></v-subheader>
+
+        <v-divider
+          v-else-if="item.divider"
+          :key="index"
+          :inset="item.inset"
+        ></v-divider>
+
+        <v-list-item
+          v-else
+          :key="item.title"
+        >
+
+          <v-list-item-content>
+            <v-list-item-title v-html="item.title"></v-list-item-title>
+            <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
+  </v-card>
+</template>
+<script>
+  export default {
+    data: () => ({
+      items: [
+        { header: 'お知らせ' },
+        {
+          title: 'Brunch this weekend?',
+          subtitle: ` I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+        },
+        { divider: true, inset: true },
+        {
+          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+          subtitle: `to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
+        },
+        { divider: true, inset: true },
+        {
+          title: 'Oui oui',
+          subtitle: ' Do you have Paris recommendations? Have you ever been?',
+        },
+        { divider: true, inset: true },
+        {
+          title: 'Birthday gift',
+          subtitle: '  Have any ideas about what we should get Heidi for her birthday?',
+        },
+        { divider: true, inset: true },
+        {
+          title: 'Recipe to try',
+          subtitle: ' We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+        },
+      ],
+    }),
+  }
+</script>
