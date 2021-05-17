c01d7c8bc4560b49c523cfb73f1cd346452eef81
Yxy <Yxy@163.com>
2021-05-17 20:16:26 +0800

    商店详情页

---
+++ src/pages/ShopDetails.vue
@@ -0,0 +1,192 @@
+<template>
+    <v-main style="overflow: hidden;position: relative">
+        <!--  Banner  -->
+        <div>
+            <v-img max-height="200px" width="100%" src="../image/image-plp-recommend.jpg"></v-img>
+        </div>
+        <!--  商铺详情  -->
+        <v-divider class="mx-4"></v-divider>
+        <div style="position: absolute;margin:0 auto;width:95%;top: 75px;left: 2%">
+            <div style="border-radius: 10px;position: relative;background:#F5F5F5;padding: 10px">
+                <div style="width: 70%;height: 41%;margin-bottom: 2%;border-bottom: 2px solid #676a6c;display: flex">
+                    <h2 style="font-size: 25px;width: 65%;letter-spacing:5px;">{{StoreEllipsis('タイトル料理')}}</h2>
+                    <span style="width: 35%;">
+                    <span>8.8</span>
+                    <van-rate size="10" color="#ffd21e" readonly  v-model="value" />
+                    <h6 style="font-size: 2%">观众评分230人</h6>
+                </span>
+                    <van-image
+                            round
+                            style="border-radius: 10px;position: absolute;top: -17%;right: 2%;width: 28%;height: 53%"
+                            src="https://img01.yzcdn.cn/vant/cat.jpeg"
+                    />
+                </div>
+
+                <div style="width: 100%;height: 59%">
+                    <div style="font-size: 2%;display: flex;margin-bottom: 2%">
+                        <div style="width: 20%;font-size: 15px">
+                            店铺介绍
+                        </div>
+                        <div style="width: 85%;height: 55px;">
+                            {{ellipsis(title)}}
+                        </div>
+                    </div>
+                    <div style="font-size: 2%;margin-bottom: 2%"> <van-icon name="clock-o" />
+                        营业时间
+                        <span style="margin : 0 2%">00：00：00 ~~ 00：00：00</span>
+                        <span style="margin-right: 1%">更多信息</span>
+                        <van-icon name="arrow" />
+                    </div>
+                    <div style="font-size: 2%;margin-bottom: 2%;letter-spacing:2px"> <van-icon name="location-o" />
+                        {{Dzellip(diZhi)}}
+                        <div style="margin-left: 5%;letter-spacing:2px">上海浦东新区世博大道1102号</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--轮播图-->
+        <div style="margin-top: 30%">
+            <van-swipe height="150" class="my-swipe" :autoplay="3000" indicator-color="white">
+                <van-swipe-item v-for="(img ,index) in images" :key="index">
+                    <img style="height: 100%" v-lazy="img" />
+                </van-swipe-item>
+            </van-swipe>
+        </div>
+        <!--  List Detail  -->
+        <div style="margin-top: 0">
+            <van-tabs v-model="active">
+                <van-tab title="服务">
+                    <div :class=" divider == true ? '' : 'divader'">
+                        <van-card
+                                v-for="(item, dex) in 5"
+                                :key="dex"
+                                price="271.00"
+                                desc="描述信息"
+                                title="商品标题"
+                                thumb="https://img01.yzcdn.cn/vant/ipad.jpeg"
+                        >
+                            <template #desc>
+                                <div>描述信息描述信息</div>
+                                <div>描述信息描述信息</div>
+                            </template>
+                        </van-card>
+
+                    </div>
+                    <div>
+                        <van-divider v-show="divider" @click="divider = false"><van-icon name="arrow-up" />折叠</van-divider>
+                        <van-divider v-show="!divider" @click="divider = true"><van-icon name="arrow-down" />展开</van-divider>
+                    </div>
+                </van-tab>
+                <van-tab title="评论">内容 2</van-tab>
+            </van-tabs>
+        </div>
+
+    </v-main>
+</template>
+
+<script>
+    export default {
+        name: "ProductList",
+        data: () => ({
+            divider:false,
+            ifOver: false, // 文本是否超出三行，默认否
+            unfold: false, // 文本是否是展开状态 默认为收起
+            active: '服务',
+            diZhi:'上海梅赛德斯奔驰文化中心（上海世博会）',
+            value:3,
+            title:'例示商品タイトル例示商品タイトル例示商品タイトル例示商品タイトル例示商品タイトル例示商品タイトル例示商品タイトル例示商品タイトル例示商品タイトル例示商品タイトル例示商品タイトル例示商品タイトル例示商品タイトル例示商品タイトル例示商品タイトル例示商品タイトル例示商品タイトル例示商品タイトル例示商品タイトル例示商品タイトル',
+            images: [
+                'https://cdn.vuetifyjs.com/images/cards/cooking.png',
+                'https://cdn.vuetifyjs.com/images/cards/cooking.png',
+            ],
+            products : [
+                {
+                    title: '例示商品タイトル',
+                    code: '1234',
+                    subTitle: '例示商品副題',
+                    description: '例示的な商品記述',
+                    rating: 4.5,
+                    image: 'https://cdn.vuetifyjs.com/images/cards/cooking.png'
+                },
+                {
+                    title: '例示商品タイトル',
+                    code: '1234',
+                    subTitle: '例示商品副題',
+                    description: '例示的な商品記述',
+                    rating: 4.5,
+                    image: 'https://cdn.vuetifyjs.com/images/cards/cooking.png'
+                },
+                {
+                    title: '例示商品タイトル',
+                    code: '1234',
+                    subTitle: '例示商品副題',
+                    description: '例示的な商品記述',
+                    rating: 4.5,
+                    image: 'https://cdn.vuetifyjs.com/images/cards/cooking.png'
+                },
+                {
+                    title: '例示商品タイトル',
+                    code: '1234',
+                    subTitle: '例示商品副題',
+                    description: '例示的な商品記述',
+                    rating: 4.5,
+                    image: 'https://cdn.vuetifyjs.com/images/cards/cooking.png'
+                },
+                {
+                    title: '例示商品タイトル',
+                    code: '1234',
+                    subTitle: '例示商品副題',
+                    description: '例示的な商品記述',
+                    rating: 4.5,
+                    image: 'https://cdn.vuetifyjs.com/images/cards/cooking.png'
+                },
+                {
+                    title: '例示商品タイトル',
+                    code: '1234',
+                    subTitle: '例示商品副題',
+                    description: '例示的な商品記述',
+                    rating: 4.5,
+                    image: 'https://cdn.vuetifyjs.com/images/cards/cooking.png'
+                }
+            ],
+        }),
+        mounted() {
+        },
+        methods: {
+            ellipsis (value) {
+                if (!value) return ''
+                if (value.length >65) {
+                    return value.slice(0,65) + '...'
+                }
+                return value
+            },
+            StoreEllipsis (value) {
+                if (!value) return ''
+                if (value.length >7) {
+                    return value.slice(0,7) + '...'
+                }
+                return value
+            },
+            Dzellip (value) {
+                if (!value) return ''
+                if (value.length >15) {
+                    return value.slice(0,15) + '...'
+                }
+                return value
+            },
+            ClickStore(val){
+                console.log(val)
+                this.$router.push({name : 'ShopDetails'})
+            },
+        },
+    }
+</script>
+
+<style scoped>
+    .my-swipe .van-swipe-item img{
+        width: 100%;
+    }
+    .divader{
+        height: 150px;overflow: hidden
+    }
+</style>
---
+++ src/pages/storeList.vue
@@ -0,0 +1,246 @@
+<template>
+    <v-main style="overflow: hidden;">
+        <!--  Banner  -->
+        <div style="position: relative" ref="piediv">
+            <!--      <div style="-->
+            <!--      height: 300px;-->
+            <!--      background-image: url('https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=4253792690,4157255255&fm=224&gp=0.jpg')-->
+            <!--      background-size: 100%;"-->
+            <!--      />-->
+            <!--          <img style="width: 100%;height: 250px" src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=4253792690,4157255255&fm=224&gp=0.jpg" alt="">-->
+            <v-img max-height="200px" width="100%" src="../image/image-plp-recommend.jpg"></v-img>
+        </div>
+        <!--  Filter  -->
+        <v-divider class="mx-4"></v-divider>
+        <v-row class="pt-5 pl-2 pr-2">
+            <v-col cols="4">
+                <v-btn depressed outlined plain class="align-center text-center" width="90%" @click="areaFilter = !areaFilter">{{ $t('i18n.product.list.filter.options.area') }}<v-icon size="15">mdi-menu-down</v-icon></v-btn>
+                <v-bottom-sheet v-model="areaFilter">
+                    <v-sheet class="text-center" height="65%">
+                        <div class="py-3 pb-5">
+                            <v-card flat>
+                                <v-card-title>{{ $t('i18n.product.list.filter.options.area') }}</v-card-title>
+                                <v-card-text>
+                                    <v-container fluid>
+                                        <v-row>
+                                            <v-col cols="6" sm="4" md="4" v-for="(typeItem, key) in fields.area" :key="key">
+                                                <v-checkbox :label="typeItem.key" color="red" :value="typeItem.value" hide-details/>
+                                            </v-col>
+                                        </v-row>
+                                    </v-container>
+                                </v-card-text>
+                            </v-card>
+                        </div>
+                    </v-sheet>
+                </v-bottom-sheet>
+            </v-col>
+            <v-col cols="4">
+                <v-btn depressed outlined plain class="align-center text-center" width="90%" @click="typeFilter = !typeFilter">{{ $t('i18n.product.list.filter.options.type') }}<v-icon size="15">mdi-menu-down</v-icon></v-btn>
+                <v-bottom-sheet v-model="typeFilter">
+                    <v-sheet class="text-center" height="65%">
+                        <div class="py-3 pb-5">
+                            <v-card flat>
+                                <v-card-title>{{ $t('i18n.product.list.filter.options.type') }}</v-card-title>
+                                <v-card-text>
+                                    <v-container fluid>
+                                        <v-row>
+                                            <v-col cols="6" sm="4" md="4" v-for="(typeItem, key) in fields.type" :key="key">
+                                                <v-checkbox :label="typeItem.key" color="red" :value="typeItem.value" hide-details/>
+                                            </v-col>
+                                        </v-row>
+                                    </v-container>
+                                </v-card-text>
+                            </v-card>
+                        </div>
+                    </v-sheet>
+                </v-bottom-sheet>
+            </v-col>
+            <v-col cols="4">
+                <v-btn depressed outlined plain class="align-center text-center" width="90%" @click="sortFilter = !sortFilter">{{ $t('i18n.product.list.filter.options.sort') }}<v-icon size="15">mdi-menu-down</v-icon></v-btn>
+                <v-bottom-sheet v-model="sortFilter">
+                    <v-sheet class="text-center" height="65%">
+                        <div class="py-3 pb-5">
+                            <v-card flat>
+                                <v-card-title>{{ $t('i18n.product.list.filter.options.sort') }}</v-card-title>
+                                <v-card-text>
+                                    <v-container fluid>
+                                        <v-radio-group v-model="radioGroup">
+                                            <v-radio class="pt-1 pb-1" v-for="(sortItem, key) in fields.sort" :key="key" :label="sortItem.key" :value="sortItem.value"/>
+                                        </v-radio-group>
+                                    </v-container>
+                                </v-card-text>
+                            </v-card>
+                        </div>
+                    </v-sheet>
+                </v-bottom-sheet>
+            </v-col>
+        </v-row>
+        <!--  List Detail  -->
+        <!--<v-row class="pl-2 pr-2">
+            <v-col cols="6" v-for="(product, key) in products" :key="key" @click="navigateToPDP(product.code)">
+                <v-card>
+                    <v-img height="180" :src="product.image"/>
+                    <v-card-title>{{ product.title }}</v-card-title>
+                    <v-card-text>
+                        <v-row align="center" class="mx-0 pb-3">
+                            <v-rating :value="product.rating" color="amber" dense half-increments readonly size="14"/>
+                        </v-row>
+                        <h6>{{ product.subTitle }}</h6>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>-->
+        <div @click="ClickStore(product)" v-for="(product, key) in products" :key="key" style="margin-bottom: 5%">
+            <van-card
+                    desc="￥170/人"
+                    :title="product.title"
+                    :thumb="product.image"
+            >
+                <template #tags>
+                    <van-tag plain type="danger">烧烤</van-tag>
+                    <van-tag plain type="danger">曹家渡</van-tag>
+                </template>
+                <template #num>
+                    <span style="position: absolute;    right: 2%;top: 21%;">上海</span>
+                </template>
+                <template #footer>
+                    <div style="width: 100%;height: 100px;display: flex;">
+                        <div style="width: 20%;height: 100%;"></div>
+                        <div style="width: 20%;height: 100%;white-space: nowrap;overflow: hidden;margin: 1%" v-for="(item,index) in 4" :key="index">
+                            <img style="height: 60%;width: 100%;border-radius:5%;" src="../image/image-plp-recommend.jpg" alt="">
+                            <div style="height: 40%;width: 100%;text-align: center">
+                                <div style="width: 100%;height: 50%;overflow: hidden;text-overflow:ellipsis;white-space: nowrap;  ">
+                                    [人气单品]爆款</div>
+                                <div style="width: 100%;height: 50%;color: #ffd01e">￥999</div>
+                            </div>
+                        </div>
+                    </div>
+                </template>
+            </van-card>
+
+        </div>
+    </v-main>
+</template>
+
+<script>
+    export default {
+        name: "ProductList",
+        data: () => ({
+            areaFilter : false,
+            typeFilter : false,
+            sortFilter : false,
+            fields : {
+                area : [
+                    {
+                        key : '東京',
+                        value : 1
+                    },
+                    {
+                        key : '大阪',
+                        value : 2
+                    },
+                    {
+                        key : '京西.',
+                        value : 3
+                    },
+                ],
+                type : [
+                    {
+                        key : '焼肉を焼く',
+                        value : 1
+                    },
+                    {
+                        key : '飲み物.飲み物',
+                        value : 2
+                    },
+                    {
+                        key : 'コーヒー.コーヒー',
+                        value : 3
+                    },
+                ],
+                sort : [
+                    {
+                        key : '売り上げごとに',
+                        value : 1
+                    },
+                    {
+                        key : '値段によって.',
+                        value : 2
+                    },
+                    {
+                        key : '距離を押す',
+                        value : 3
+                    },
+                ]
+            },
+            products : [
+                {
+                    title: '例示商品タイトル',
+                    code: '1234',
+                    subTitle: '例示商品副題',
+                    description: '例示的な商品記述',
+                    rating: 4.5,
+                    image: 'https://cdn.vuetifyjs.com/images/cards/cooking.png'
+                },
+                {
+                    title: '例示商品タイトル',
+                    code: '1234',
+                    subTitle: '例示商品副題',
+                    description: '例示的な商品記述',
+                    rating: 4.5,
+                    image: 'https://cdn.vuetifyjs.com/images/cards/cooking.png'
+                },
+                {
+                    title: '例示商品タイトル',
+                    code: '1234',
+                    subTitle: '例示商品副題',
+                    description: '例示的な商品記述',
+                    rating: 4.5,
+                    image: 'https://cdn.vuetifyjs.com/images/cards/cooking.png'
+                },
+                {
+                    title: '例示商品タイトル',
+                    code: '1234',
+                    subTitle: '例示商品副題',
+                    description: '例示的な商品記述',
+                    rating: 4.5,
+                    image: 'https://cdn.vuetifyjs.com/images/cards/cooking.png'
+                },
+                {
+                    title: '例示商品タイトル',
+                    code: '1234',
+                    subTitle: '例示商品副題',
+                    description: '例示的な商品記述',
+                    rating: 4.5,
+                    image: 'https://cdn.vuetifyjs.com/images/cards/cooking.png'
+                },
+                {
+                    title: '例示商品タイトル',
+                    code: '1234',
+                    subTitle: '例示商品副題',
+                    description: '例示的な商品記述',
+                    rating: 4.5,
+                    image: 'https://cdn.vuetifyjs.com/images/cards/cooking.png'
+                }
+            ],
+            radioGroup: 1,
+        }),
+        methods: {
+            ClickStore(val){
+                console.log(val)
+                this.$router.push({name : 'ShopDetails'})
+            },
+            reserve () {
+                this.loading = true
+                setTimeout(() => (this.loading = false), 2000)
+            },
+            navigateToPDP (itemCode) {
+                this.$router.push({name : 'product-detail', params: {'code': itemCode}})
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
