51bd06153e15ac24267d5297a1685c096227a745
Yxy <Yxy@163.com>
2021-06-28 14:51:12 +0800

    0628

--- src/pages/ShopDetails.vue
+++ src/pages/ShopDetails.vue
@@ -1,15 +1,17 @@
 <template>
-    <v-main style="overflow: hidden;position: relative" >
+    <v-main style="overflow: hidden;position: relative">
         <!--  Banner  -->
         <div style="position: relative">
             <v-img max-height="20%" width="100%" src="../image/image-plp-recommend.jpg"></v-img>
         </div>
         <!--轮播图-->
         <div>
-            <van-empty v-if="images.length <= 0" description="別のキーワードを試すか、検索フィルターを削除してください" />
-            <van-swipe v-if="images.length > 0" height="250" class="my-swipe" style="border-radius: 10px;width: 95%; margin-top: 2%;margin-left: 2%" :autoplay="3000" indicator-color="white">
+            <van-empty v-if="images.length <= 0" description="別のキーワードを試すか、検索フィルターを削除してください"/>
+            <van-swipe v-if="images.length > 0" height="250" class="my-swipe"
+                       style="border-radius: 10px;width: 95%; margin-top: 2%;margin-left: 2%" :autoplay="3000"
+                       indicator-color="white">
                 <van-swipe-item v-for="(img ,index) in images" :key="index">
-                    <img style="height: 100%" v-lazy="img" />
+                    <img style="height: 100%" v-lazy="img"/>
                 </van-swipe-item>
             </van-swipe>
         </div>
@@ -30,7 +32,8 @@
                             {{ellipsis(details.description)}}
                         </div>
                     </div>
-                    <div style="font-size: 2%;margin-bottom: 2%;font-size: 15px"> <van-icon name="clock-o" />
+                    <div style="font-size: 2%;margin-bottom: 2%;font-size: 15px">
+                        <van-icon name="clock-o"/>
                         営業時間
                         <span style="margin : 0 2%;font-size: 15px">{{details.openTime}} ~~ {{details.closeTime}}</span>
                         <!--                            <span style="margin-right: 1%">詳しくは</span>-->
@@ -38,11 +41,17 @@
                     </div>
                     <div style="font-size: 2%;margin-bottom: 2%;letter-spacing:2px">
 
-                        <div style="letter-spacing:2px;font-size: 15px"> <van-icon name="location-o" /> {{(details.location)}}</div>
+                        <div style="letter-spacing:2px;font-size: 15px">
+                            <van-icon name="location-o"/>
+                            {{(details.location)}}
+                        </div>
                     </div>
                     <div style="font-size: 2%;margin-bottom: 2%;letter-spacing:2px">
 
-                        <div style="letter-spacing:2px;font-size: 15px"> <van-icon name="location-o" /> {{Dzellip(details.address)}}</div>
+                        <div style="letter-spacing:2px;font-size: 15px">
+                            <van-icon name="location-o"/>
+                            {{Dzellip(details.address)}}
+                        </div>
                     </div>
                 </div>
             </div>
@@ -51,25 +60,27 @@
         <div style="margin-top: 0">
             <van-tabs v-model="active">
                 <van-tab title="メニュー">
-<!--                    :class=" divider == true ? '' : 'divader'"-->
-                        <div v-if="products.length > 0" style="margin-bottom: 3%;">
-                            <van-card
-                                    style="border-radius: 10px;box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%);width: 95%;margin-left: 2%"
-                                    @click="navigateToPDP(item)"
-                                    v-for="(item, dex) in products"
-                                    :key="dex"
-                                    :price="item.prices"
-                                    :desc="item.title"
-                                    :title="item.name"
-                                    :thumb="item.logoImage.target"
-                            >
-                                <template #desc>
-                                    <div>{{item.subTitle}}</div>
-                                    <textarea style="    width: 100%;" readonly :placeholder="item.description"></textarea>
-                                </template>
-                            </van-card>
+                    <!--                    :class=" divider == true ? '' : 'divader'"-->
+                    <div v-if="products.length > 0" style="margin-bottom: 3%;">
+                        <van-card
+                                style="border-radius: 10px;box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%);width: 95%;margin-left: 2%"
+                                @click="navigateToPDP(item)"
+                                v-for="(item, dex) in products"
+                                :key="dex"
+                                :desc="item.title"
+                                :title="item.name"
+                                :thumb="item.logoImage.target"
+                        >
+                            <template #price>
+                                <div style="font-size: 16px; font-family: Avenir-Heavy,PingFang SC,Helvetica Neue,Arial,sans-serif;">{{item.prices}} 円</div>
+                            </template>
+                            <template #desc>
+                                <div>{{item.subTitle}}</div>
+                                <textarea style="    width: 100%;" readonly :placeholder="item.description"></textarea>
+                            </template>
+                        </van-card>
 
-                        </div>
+                    </div>
                     <!--<div v-if="products.length > 1">
                         <van-divider v-show="divider" @click="divider = false"><van-icon name="arrow-up" />折りたたむ</van-divider>
                         <van-divider v-show="!divider" @click="divider = true"><van-icon name="arrow-down" />展開する</van-divider>
@@ -95,7 +106,8 @@
                                                 :placeholder="item.comment"
                                                 show-word-limit
                                         />
-                                        <div v-if="item.delete == true" @click="commDel(item.id)" style="color: red;float: right;margin-right: 5%;">
+                                        <div v-if="item.delete == true" @click="commDel(item.id)"
+                                             style="color: red;float: right;margin-right: 5%;">
                                             削除
                                         </div>
                                         <van-field
@@ -123,69 +135,75 @@
                                 </div>
 
                                 <div v-if="items.length > 1">
-                                    <van-divider v-show="dividers" @click="dividers = false"><van-icon name="arrow-up" />コメントを隠す</van-divider>
-                                    <van-divider v-show="!dividers" @click="dividers = true"><van-icon name="arrow-down" />
-                                        {{items.length}}コメント</van-divider>
+                                    <van-divider v-show="dividers" @click="dividers = false">
+                                        <van-icon name="arrow-up"/>
+                                        コメントを隠す
+                                    </van-divider>
+                                    <van-divider v-show="!dividers" @click="dividers = true">
+                                        <van-icon name="arrow-down"/>
+                                        {{items.length}}コメント
+                                    </van-divider>
                                 </div>
                             </v-card>
                         </v-col>
 
                     </v-row>
-                   <div v-if="idcomment" style="margin: auto;width: 90%">
-                           <van-field
-                                   v-model="message"
-                                   rows="2"
-                                   autosize
-                                   label="コメント"
-                                   type="textarea"
-                                   maxlength="100"
+                    <div v-if="idcomment" style="margin: auto;width: 90%">
+                        <van-field
+                                v-model="message"
+                                rows="2"
+                                autosize
+                                label="コメント"
+                                type="textarea"
+                                maxlength="100"
 
-                                   placeholder="コメントを入力してください"
-                                   show-word-limit
-                           >
-                               <template #button>
-                                   <van-button style="float: right" @click="pinLun" round type="info">送信</van-button>
-                               </template>
-                           </van-field>
-                       <van-rate v-model="value"
-                                 :size="25"
-                                 color="#ffd21e"
-                                 void-icon="star"
-                                 void-color="#eee"
-                       />
+                                placeholder="コメントを入力してください"
+                                show-word-limit
+                        >
+                            <template #button>
+                                <van-button style="float: right" @click="pinLun" round type="info">送信</van-button>
+                            </template>
+                        </van-field>
+                        <van-rate v-model="value"
+                                  :size="25"
+                                  color="#ffd21e"
+                                  void-icon="star"
+                                  void-color="#eee"
+                        />
 
-                       </div>
+                    </div>
                 </van-tab>
 
             </van-tabs>
         </div>
         <van-overlay :show="show">
             <div class="wrapper">
-                <van-loading color="#1989fa" />
+                <van-loading color="#1989fa"/>
             </div>
         </van-overlay>
     </v-main>
 </template>
 
 <script>
-    import { Notify } from 'vant';
-    import {shopId,serviceShop,comment,commentPinlun,commentDel,isComment} from "@api"
+    import {Notify} from 'vant';
+    import {shopId, serviceShop, comment, commentPinlun, commentDel, isComment} from "@api"
+
     export default {
         name: "ProductList",
         data: () => ({
-            message:'',
-            show:true,
-            divider:false,
-            dividers:false,
+            message: '',
+            show: true,
+            divider: false,
+            dividers: false,
             ifOver: false, // 文本是否超出三行，默认否
             unfold: false, // 文本是否是展开状态 默认为收起
             active: 'サービス',
-            value:0,
+            value: 0,
             images: [],
             items: [],
-            products : [],
-            details:{},
-            idcomment:false
+            products: [],
+            details: {},
+            idcomment: false
         }),
 
         mounted() {
@@ -194,26 +212,26 @@
         created() {
             var that = this
             isComment({
-                shopId:that.$route.query.id,
+                shopId: that.$route.query.id,
             })
                 .then(function (response) {
                     that.idcomment = response.data
                 })
                 .catch(function (error) {
-                    Notify({ type: 'warning', message: error.errMessage });
+                    Notify({type: 'warning', message: error.errMessage});
                 });
             comment({
-                serviceId:that.$route.query.id,
+                serviceId: that.$route.query.id,
             })
                 .then(function (response) {
                     that.items = response.data
                     that.show = false
                 })
                 .catch(function (error) {
-                    Notify({ type: 'warning', message: error.errMessage });
+                    Notify({type: 'warning', message: error.errMessage});
                 });
             serviceShop({
-                shopId:that.$route.query.id,
+                shopId: that.$route.query.id,
             })
                 .then(function (response) {
                     that.products = response.data
@@ -221,115 +239,118 @@
                     that.show = false
                 })
                 .catch(function (error) {
-                    Notify({ type: 'warning', message: error.errMessage });
+                    Notify({type: 'warning', message: error.errMessage});
                 });
             shopId({
-                id:that.$route.query.id,
+                id: that.$route.query.id,
             })
                 .then(function (response) {
                     that.details = response.data
-                    response.data.imageUrls.forEach(ele=>{
-                        if(ele.type == 'BANNER'){
+                    response.data.imageUrls.forEach(ele => {
+                        if (ele.type == 'BANNER') {
                             that.images.push(ele.target)
                         }
                     })
                     that.show = false
                 })
                 .catch(function (error) {
-                    Notify({ type: 'warning', message: error.errMessage });
+                    Notify({type: 'warning', message: error.errMessage});
                 });
         },
         methods: {
-            commDel(id){
+            commDel(id) {
                 var that = this
                 commentDel(id)
-                    .then((res)=> {
-                        Notify({ type: 'success', message: '削除されました！' });
+                    .then((res) => {
+                        Notify({type: 'success', message: '削除されました！'});
                         comment({
-                            serviceId:that.$route.query.id,
+                            serviceId: that.$route.query.id,
                         })
                             .then(function (response) {
                                 that.items = response.data
                                 that.show = false
                             })
                             .catch(function (error) {
-                                Notify({ type: 'warning', message: error.errMessage });
+                                Notify({type: 'warning', message: error.errMessage});
                             });
                         console.log(res)
                     }).catch(function (error) {
-                    Notify({ type: 'warning', message: error.errMessage });
+                    Notify({type: 'warning', message: error.errMessage});
                 });
             },
-            pinLun(){
+            pinLun() {
 
                 // console.log(this.$store.state.token)
-                if(this.message == ''){
+                if (this.message == '') {
                     return
                 }
                 var that = this
 
                 commentPinlun({
-                    serviceId:that.$route.query.id,
-                    star:that.value,
-                    comment:that.message
+                    serviceId: that.$route.query.id,
+                    star: that.value,
+                    comment: that.message
                 })
-                    .then((res)=> {
+                    .then((res) => {
                         console.log(res)
                         that.message = ''
                         comment({
-                            serviceId:that.$route.query.id,
+                            serviceId: that.$route.query.id,
                         })
                             .then(function (response) {
                                 that.items = response.data
 
                             })
                             .catch(function (error) {
-                                Notify({ type: 'warning', message: error.errMessage });
+                                Notify({type: 'warning', message: error.errMessage});
                             });
                     })
                     .catch(function (error) {
-                        Notify({ type: 'warning', message: error.errMessage });
+                        Notify({type: 'warning', message: error.errMessage});
                     });
             },
-            navigateToPDP (val) {
-                this.$router.push({name : 'product',query:{id:val.id}})
+            navigateToPDP(val) {
+                this.$router.push({name: 'product', query: {id: val.id}})
             },
-            ellipsis (value) {
+            ellipsis(value) {
                 if (!value) return ''
-                if (value.length >120) {
-                    return value.slice(0,120) + '...'
+                if (value.length > 120) {
+                    return value.slice(0, 120) + '...'
                 }
                 return value
             },
-            StoreEllipsis (value) {
+            StoreEllipsis(value) {
                 if (!value) return ''
-                if (value.length >12) {
-                    return value.slice(0,12) + '...'
+                if (value.length > 12) {
+                    return value.slice(0, 12) + '...'
                 }
                 return value
             },
-            Dzellip (value) {
+            Dzellip(value) {
                 if (!value) return ''
-                if (value.length >15) {
-                    return value.slice(0,15) + '...'
+                if (value.length > 15) {
+                    return value.slice(0, 15) + '...'
                 }
                 return value
             },
-            ClickStore(val){
+            ClickStore(val) {
                 console.log(val)
-                this.$router.push({name : 'ShopDetails'})
+                this.$router.push({name: 'ShopDetails'})
             },
         },
     }
 </script>
 
 <style scoped>
-    .my-swipe .van-swipe-item img{
+    .my-swipe .van-swipe-item img {
         width: 100%;
     }
-    .divader{
-        height: 150px;overflow: hidden
+
+    .divader {
+        height: 150px;
+        overflow: hidden
     }
+
     .wrapper {
         display: flex;
         align-items: center;
