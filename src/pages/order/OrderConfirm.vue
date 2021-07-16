5147418842df0bada8f072864bea659994b2d964
dingwei <625073235@qq.com>
2021-07-16 22:29:51 +0800

    代码修正

--- src/pages/order/OrderConfirm.vue
+++ src/pages/order/OrderConfirm.vue
@@ -55,16 +55,16 @@
         </van-overlay>
     </v-main>
     
-    <div id="smart-button-container"> <div style="text-align: center;"> <div id="paypal-button-container"></div> </div> </div>
+    
     
 </template>
-  
+
 <script>
 
     // import axios from "axios";
     import {payfor,order, sessions, service} from '@api'
     import {Notify} from "vant";
-import { paypal } from 'https://www.paypal.com/sdk/js?client-id=sb&enable-funding=venmo&currency=JPY'
+
     export default {
         name: "OrderConfirm",
         data() {
@@ -81,34 +81,6 @@ import { paypal } from 'https://www.paypal.com/sdk/js?client-id=sb&enable-fundin
             }
         },
         created() {
-         function initPayPalButton() {
-      paypal.Buttons({
-        style: {
-          shape: 'rect',
-          color: 'gold',
-          layout: 'horizontal',
-          label: 'paypal',
-          
-        },
-
-        createOrder: function(data, actions) {
-          return actions.order.create({
-            purchase_units: [{"description":"123","amount":{"currency_code":"JPY","value":213123}}]
-          });
-        },
-
-        onApprove: function(data, actions) {
-          return actions.order.capture().then(function(details) {
-            alert('Transaction completed by ' + details.payer.name.given_name + '!');
-          });
-        },
-
-        onError: function(err) {
-          console.log(err);
-        }
-      }).render('#paypal-button-container');
-    }
-    initPayPalButton();
             var that = this
             service({
                 id: that.$route.query.id,
